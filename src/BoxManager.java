02df31203c74a7ae29c833cc991f63b04692f323
TJHL <stephen.cerruti@jointheleague.org>
2016-12-05 18:33:36 -0800

    Line of red

--- src/BoxManager.java
+++ src/BoxManager.java
@@ -5,16 +5,35 @@ import java.util.ArrayList;
 public class BoxManager {
 	Box fat;
 	ArrayList<Box> Fodder;
-	BoxManager(Color color, int height, int width,int x,int y, int speedx, int speedy, boolean alive){
+	
+	BoxManager(){
 		fat= new Box(Color.BLACK,40,40,400,400,5,5,true);
 		Fodder = new ArrayList<Box>();
 		
 	
 	}
-	void Update(){
-	fat.update();
-	}
-	void draw(Graphics g){
-		fat.draw(g);
-	}
+		void Update(){
+			fat.update();
+			for (int i = 0; i < Fodder.size(); i++) {
+				Box spawn=Fodder.get(i);
+				spawn.update();
+			}
+			manageFodder();
+		}
+		void draw(Graphics g){
+			fat.draw(g);
+			for (int i = 0; i < Fodder.size(); i++) {
+				Box spawn=Fodder.get(i);
+				spawn.draw(g);
+			}
+		}
+		
+		void addFodder(Box lil){
+			Fodder.add(lil);
+			
+		}
+		void manageFodder(){
+			Box spawn= new Box(Color.RED,20,20,100,100,5,9,true);
+			addFodder(spawn);
+		}
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,7 +2,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -10,28 +9,29 @@ public class GamePanel extends JPanel implements ActionListener{
 	Timer clock;
 	Box Fodder;
 	Box fat;
-	GamePanel(){
 	BoxManager manager;
-	clock = new Timer(1000 / 60,(ActionListener) this);
-	Fodder = new Box(Color.RED,20,20,100,100,5,9,true);
-	fat= new Box(Color.BLACK,40,40,400,400,5,5,true);
-	
-	}
-	public void paintComponent(Graphics g){
 
-		Fodder.draw(g);
-		fat.draw(g);
-		
-	}
+	GamePanel(){
+		clock = new Timer(1000 / 60,(ActionListener) this);
+		Fodder = new Box(Color.RED,20,20,100,100,5,9,true);
+		fat= new Box(Color.BLACK,40,40,400,400,5,5,true);
+		manager= new BoxManager();
 	
-	void startGame(){
-		clock.start();
+		}
+			public void paintComponent(Graphics g){
+				manager.draw(g);
 		
-	}
-	public void actionPerformed(ActionEvent e) {
-		repaint();
-		Fodder.update();
-		fat.update();
+			}
+	
+			void startGame(){
+				clock.start();
 		
-	}
+			}
+			
+			public void actionPerformed(ActionEvent e) {
+				repaint();
+				manager.Update();
+		
+		
+			}
 } 
