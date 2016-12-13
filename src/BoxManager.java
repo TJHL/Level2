13eeb2c905fe9a7aebf2bc0abbeb08f19b525921
TJHL <league@WTS7.attlocal.net>
2016-12-12 18:29:56 -0800

    FODDER DEATH

--- src/BoxManager.java
+++ src/BoxManager.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class BoxManager {
 	Box fat;
 	ArrayList<Box> Fodder;
+	int fodderTimer;
+	int fodderSpawnTimer=20;
 	
 	BoxManager(){
 		fat= new Box(Color.BLACK,40,40,400,400,5,5,true);
@@ -19,21 +21,49 @@ public class BoxManager {
 				spawn.update();
 			}
 			manageFodder();
+			checkFodder();
+			killFodder();
+			
 		}
 		void draw(Graphics g){
-			fat.draw(g);
 			for (int i = 0; i < Fodder.size(); i++) {
 				Box spawn=Fodder.get(i);
 				spawn.draw(g);
 			}
+			fat.draw(g);
 		}
+		
 		
 		void addFodder(Box lil){
 			Fodder.add(lil);
 			
 		}
 		void manageFodder(){
-			Box spawn= new Box(Color.RED,20,20,100,100,5,9,true);
+			fodderTimer+=1;
+			if(fodderTimer>= fodderSpawnTimer){
+			fodderTimer=0;
+			Box spawn= new Box(Color.RED,20,20,(int)(Math.random()*Box_Collision.WIDTH),(int)(Math.random()*Box_Collision.HEIGHT),5,9,true);
 			addFodder(spawn);
+			}
+	
+		}
+		void checkFodder(){
+			for (int i = 0; i < Fodder.size(); i++) {
+				checkEat(fat, Fodder.get(i));
+			}
+		}
+		void checkEat(Box A, Box B){
+			if((A.x<=B.x)&&(A.x+A.width >= B.x+B.width)&&(A.y<=B.y)&&(A.y+A.height>=B.y+B.height)){
+				B.alive=false;
+			}
+		}
+		void killFodder(){
+			for (int i = 0; i < Fodder.size(); i++) {
+				if(Fodder.get(i).isalive()== false){
+					Fodder.remove(i);
+				}
+			}
+				
+			
 		}
 }
